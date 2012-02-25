6eecd7ed266acb9e5709ca57e53dcb7d4510a4a3
Benoit Bayol <benoit.bayol@gmail.com>
2012-02-25 14:36:58 +0700

    bbayol

--- CodingDojo23Test.cpp
+++ CodingDojo23Test.cpp
@@ -8,8 +8,17 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-BOOST_AUTO_TEST_CASE(test1)
+void player1_win(int & player1_score, int & player2_score)
 {
+    player1_score = 15;
+}
+
+BOOST_AUTO_TEST_CASE(score_is_0_0_player1_wins_then_15_0)
+{
+    int player1_score = 0;
+    int player2_score = 0;
+    player1_win(player1_score, player2_score);
+    BOOST_CHECK(player1_score == 15);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
