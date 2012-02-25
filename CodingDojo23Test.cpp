758a11b97d0164e97175e93c46f6c7b9e5744ab1
Benoit Bayol <benoit.bayol@gmail.com>
2012-02-25 14:43:25 +0700

    cmpitg

--- CodingDojo23Test.cpp
+++ CodingDojo23Test.cpp
@@ -8,21 +8,24 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
 
-void player1_win(int & player1_score, int & player2_score)
-{
-    player1_score = 15;
-}
 
-void player2_win(int & player1_score, int & player2_score)
+void player_win(int & player1_score, int & player2_score, int player_number)
 {
-    player2_score = 15;
+    if(player_number == 0)
+        {
+            player1_score = 15;
+        }
+    else
+        {
+            player2_score = 15;
+        }
 }
 
 BOOST_AUTO_TEST_CASE(score_is_0_0_player1_wins_then_15_0)
 {
     int player1_score = 0;
     int player2_score = 0;
-    player1_win(player1_score, player2_score);
+    player_win(player1_score, player2_score, 0);
     BOOST_CHECK(player1_score == 15);
 }
 
@@ -30,7 +33,7 @@ BOOST_AUTO_TEST_CASE(score_is_0_0_player2_wins_then_0_15)
 {
     int player1_score = 0;
     int player2_score = 0;
-    player2_win(player1_score, player2_score);
+    player_win(player1_score, player2_score, 1);
     BOOST_CHECK(player2_score == 15);
 }
 
