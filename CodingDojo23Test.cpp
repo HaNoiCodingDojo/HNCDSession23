cc5a4d3e1e4c535ab088d6a45c7d7eced8529e67
Benoit Bayol <benoit.bayol@gmail.com>
2012-02-25 14:31:29 +0700

    bbayol

---
+++ CodingDojo23Test.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_MODULE CodingDojo18
+#include <boost/test/included/unit_test.hpp>
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(BOOST_TEST_MODULE)
+
+BOOST_AUTO_TEST_CASE(test1)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#undef BOOST_TEST_MODULE
+
